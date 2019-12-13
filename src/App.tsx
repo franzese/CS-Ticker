2849152857a70b5d54cd3e2fccca71bdfc814434
Ricky Franzese <thechosenrick@gmail.com>
2019-12-13 12:27:22 -0500

    Converting project to TypScript and SASS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from 'src/assets/logo.svg';
+
+function App() {
+    return (
+        <div className="App p-4">
+            <h1>Stocks</h1>
+            <img className="mb-4" src={logo} alt="logo" />
+        </div>
+    );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'src/styles/common.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
