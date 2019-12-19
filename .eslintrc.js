08f08f0a43faa86c1343880c9beb2c63b4d4f851
Ricky Franzese <thechosenrick@gmail.com>
2019-12-19 11:04:50 -0500

    Root project fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,11 @@ module.exports = {
   extends: ['react-app', 'airbnb'],
   rules: {
     'react/jsx-filename-extension': 0,
-  },
+    'no-else-return': 0,
+    'comma-dangle': true,
+    tabWidth: 2,
+    useTabs: false,
+    'editor.formatOnSave': true,
+    singleQuote: true
+  }
 };
